934b2408ddcfed9a83d89c91960a112c63f68bb3
riteshthakre1 <riteshthakre976@gmail.com>
2025-12-16 23:10:59 +0530

    [Ritesh] Added: WelcomeToBridgelabz.java

---
+++ gcr-codebase/programming-prerequisites/WelcomeToBridgelabz.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class WelcomeToBridgelabz{
+	public static void main(String [] args){
+		Scanner sc = new Scanner(System.in);
+		//String name= sc.next();
+		System.out.println("Welcome to Bridgelabz!");
+	}
+}
