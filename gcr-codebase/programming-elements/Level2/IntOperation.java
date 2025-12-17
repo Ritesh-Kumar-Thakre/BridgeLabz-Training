8a59976c00a559cbbcbbae480c44770fe0bc4d48
riteshthakre1 <riteshthakre976@gmail.com>
2025-12-18 03:39:42 +0530

    [Ritesh] : added IntOperation.java in Level2

---
+++ gcr-codebase/programming-elements/Level2/IntOperation.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class IntOperation{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		// Taking User Inputs
+		System.out.println("Enter three numbers:");
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int c=sc.nextInt();
+		
+		// Applying the required logic
+		int result1=a+(b*c);
+		int result2=(a*b)+c;
+		int result3=c+(a/b);
+		int result4=(a%b)+c;
+		
+		// Printing the results
+		System.out.println("The results of Int Operations are "+result1+", "+ result2+", "+result3+", and "+result4);
+	}
+}
