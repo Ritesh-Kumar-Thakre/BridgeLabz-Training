40a95a7bace3f9b5ed1449c66758bfd61a08f381
riteshthakre1 <riteshthakre976@gmail.com>
2025-12-19 00:19:30 +0530

    [Ritesh] : added LeapYearChecker2.java in Level3

---
+++ gcr-codebase/control-flow/Level3/LeapYearChecker2.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class LeapYearChecker2 {
+	public static void main(String []args){
+		Scanner sc = new Scanner(System.in);
+		// Taking User Inputs
+		System.out.println("Enter year:");
+		int year= sc.nextInt();
+		boolean isLeapYear=false;
+		
+		// Applying the required logic
+		if(year>=1582){
+			if((year%4==0&&year%100!=0)||(year%400==0)){
+				isLeapYear=true;
+			}
+		}else{
+			System.out.println("Enter year >= 1582");
+		}
+		
+		// Printing the results
+		if(isLeapYear){
+			System.out.println("Year "+year+" is a Leap Year");
+		}else{
+			System.out.println("Year "+year+" is not a Leap Year");
+		}
+		sc.close();
+	}
+}
