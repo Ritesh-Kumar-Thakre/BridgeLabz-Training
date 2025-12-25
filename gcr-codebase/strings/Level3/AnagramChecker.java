fc693556c41e1140c4ecc76bd3d9a486d5a418ee
riteshthakre1 <riteshthakre976@gmail.com>
2025-12-25 22:22:38 +0530

    [Ritesh] : added AnagramChecker.java in Level3

---
+++ gcr-codebase/strings/Level3/AnagramChecker.java
@@ -0,0 +1,35 @@
+import java.util.*;
+public class AnagramChecker {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first text:");
+        String text1 = sc.nextLine();
+        System.out.println("Enter second text:");
+        String text2 = sc.nextLine();
+
+        boolean result = isAnagram(text1, text2);
+        System.out.println("\nAre the two texts anagrams? " + result);
+        sc.close();
+    }
+
+    // Method to check if two strings are anagrams
+    public static boolean isAnagram(String text1, String text2){
+        if(text1.length() != text2.length()){
+            return false;
+        }
+        int[] freq1 = new int[256];
+        int[] freq2 = new int[256];
+        for(int i=0;i<text1.length();i++){
+            char c1 = text1.charAt(i);
+            char c2 = text2.charAt(i);
+            freq1[c1]++;
+            freq2[c2]++;
+        }
+        for(int i=0;i<256;i++){
+            if(freq1[i] != freq2[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+}
