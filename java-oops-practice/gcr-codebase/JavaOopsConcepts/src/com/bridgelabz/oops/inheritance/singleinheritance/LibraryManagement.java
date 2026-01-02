925dca3584d0efea4aca346b006f72a2758bfc90
riteshthakre1 <riteshthakre976@gmail.com>
2026-01-02 16:12:28 +0530

    [Ritesh] : removed singleinheritance folder

--- java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/bridgelabz/oops/inheritance/singleinheritance/LibraryManagement.java
+++
@@ -1,8 +0,0 @@
-package com.bridgelabz.oops.inheritance.singleinheritance;
-
-public class LibraryManagement {
-	public static void main(String[] args) {
-		Author a1 = new Author("Ritesh Thakre", "AI & Java Developer", "Core Java Concepts", 2025);
-		a1.displayInfo();
-	}
-}
--- java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/bridgelabz/oops/inheritance/singleinheritance/Thermostat.java
+++
@@ -1,5 +0,0 @@
-package com.bridgelabz.oops.inheritance.singleinheritance;
-
-public class Thermostat {
-
-}
