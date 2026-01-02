5f17294bb4f3ad53a827ef92e54770c5b6a2f121
riteshthakre1 <riteshthakre976@gmail.com>
2026-01-02 15:24:38 +0530

    [Ritesh] : added Faculty.java in universityproblem

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/bridgelabz/oops/objectmodeling/universityproblem/Faculty.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.oops.objectmodeling.universityproblem;
+
+public class Faculty {
+    private int facultyId;
+    private String name;
+
+    public Faculty(int facultyId, String name) {
+        this.facultyId = facultyId;
+        this.name = name;
+    }
+
+    public void showFaculty() {
+        System.out.println("Faculty ID: " + facultyId + ", Name: " + name);
+    }
+}
