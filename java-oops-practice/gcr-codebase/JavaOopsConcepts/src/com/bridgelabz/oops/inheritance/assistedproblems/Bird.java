dc8ebfdbddcaefb5b0094c14234e7d45d37ad0f6
riteshthakre1 <riteshthakre976@gmail.com>
2026-01-02 16:07:25 +0530

    [Ritesh] : added assistedproblems in inheritance and Animal related files in assistedproblems

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/bridgelabz/oops/inheritance/assistedproblems/Bird.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.oops.inheritance;
+
+public class Bird extends Animal {
+	public Bird(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println(name + " says: Chirp! Chirp!");
+    }
+}
