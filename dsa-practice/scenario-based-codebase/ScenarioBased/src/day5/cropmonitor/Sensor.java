b5044875b4729936ed36771f4ff022ee173a9adc
riteshthakre1 <riteshthakre976@gmail.com>
2026-01-20 12:41:24 +0530

    [Ritesh] : added cropmonitor folder and sub files in day5

---
+++ dsa-practice/scenario-based-codebase/ScenarioBased/src/day5/cropmonitor/Sensor.java
@@ -0,0 +1,12 @@
+package day5.cropmonitor;
+
+public class Sensor {
+
+	int timeStamp;
+	double temp;
+
+	Sensor(int timeStamp, double temp) {
+		this.timeStamp = timeStamp;
+		this.temp = temp;
+	}
+}
