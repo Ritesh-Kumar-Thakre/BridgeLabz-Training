54be0489546676c14b05245fa38a01f0ab285484
riteshthakre1 <riteshthakre976@gmail.com>
2026-01-20 12:37:33 +0530

    [Ritesh] : added ExamCell folder and sub files in day4

---
+++ dsa-practice/scenario-based-codebase/ScenarioBased/src/day4/ExamCell/Scores.java
@@ -0,0 +1,11 @@
+package day4.ExamCell;
+
+public class Scores {
+	String studentName;
+	double score;
+
+	Scores(String studentName, double score) {
+		this.studentName = studentName;
+		this.score = score;
+	}
+}
