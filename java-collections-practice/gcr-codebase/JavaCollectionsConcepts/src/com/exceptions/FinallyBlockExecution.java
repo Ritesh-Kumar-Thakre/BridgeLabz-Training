5a99c85f6d98b85e89c973e58e27e4853f910304
riteshthakre1 <riteshthakre976@gmail.com>
2026-01-27 12:39:39 +0530

    [Ritesh]  : added FinallyBlockExecution.java file in exceptions

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsConcepts/src/com/exceptions/FinallyBlockExecution.java
@@ -0,0 +1,31 @@
+package com.exceptions;
+
+import java.util.Scanner;
+
+public class FinallyBlockExecution {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter first number: ");
+		int a = sc.nextInt();
+
+		System.out.println("Enter second number: ");
+		int b = sc.nextInt();
+
+		try {
+			int result = a / b;
+			System.out.println("Result: " + result);
+
+		} catch (ArithmeticException e) {
+			System.out.println("Cannot divide by zero");
+
+		} finally {
+			System.out.println("Operation completed");
+		}
+
+		sc.close();
+	}
+
+}
