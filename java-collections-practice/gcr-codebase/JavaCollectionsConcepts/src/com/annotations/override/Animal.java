343f10ea888d1c0b1e42b98b5a573859e62f83e2
riteshthakre1 <riteshthakre976@gmail.com>
2026-01-27 16:15:41 +0530

    [Ritesh] : added override folder and sub files in annotations

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsConcepts/src/com/annotations/override/Animal.java
@@ -0,0 +1,7 @@
+package com.annotations.override;
+
+public class Animal {
+	void makeSound() {
+		System.out.println("Animal sound");
+	}
+}
---
+++ java-collections-practice/gcr-codebase/JavaCollectionsConcepts/src/com/annotations/override/Test.java
@@ -0,0 +1,8 @@
+package com.annotations.override;
+
+public class Test {
+	public static void main(String[] args) {
+		Animal a = new Dog();
+		a.makeSound();
+	}
+}
