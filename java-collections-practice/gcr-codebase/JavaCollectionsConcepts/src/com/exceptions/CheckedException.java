5c88d483b92e674345b219646770648f72aa7e43
riteshthakre1 <riteshthakre976@gmail.com>
2026-01-27 12:35:35 +0530

    [Ritesh]  : added CheckedException.java file in exceptions

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsConcepts/src/com/exceptions/CheckedException.java
@@ -0,0 +1,20 @@
+package com.exceptions;
+
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class CheckedException {
+	public static void main(String[] args) {
+		try (BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\acer\\OneDrive\\Desktop\\csvfile.txt"))) {
+			String line;
+			while ((line = br.readLine()) != null) {
+				System.out.println(line);
+			}
+		} catch (IOException e) {
+			System.out.println("File not found");
+		}
+	}
+
+}
