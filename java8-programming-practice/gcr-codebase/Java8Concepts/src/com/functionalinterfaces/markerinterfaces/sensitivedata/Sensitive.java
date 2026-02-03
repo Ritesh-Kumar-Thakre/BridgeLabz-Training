ea514ae20ddcba0c4dc0570f55e9d5147d05c2c4
riteshthakre1 <riteshthakre976@gmail.com>
2026-02-03 12:09:09 +0530

    [Ritesh] : added sensitivedata folder and sub files in markerinterfaces

---
+++ java8-programming-practice/gcr-codebase/Java8Concepts/src/com/functionalinterfaces/markerinterfaces/sensitivedata/Sensitive.java
@@ -0,0 +1,7 @@
+package com.functionalinterfaces.markerinterfaces.sensitivedata;
+
+// Custom marker interface
+// No methods – used only to tag classes with sensitive data
+public interface Sensitive {
+    // empty – pure marker
+}
