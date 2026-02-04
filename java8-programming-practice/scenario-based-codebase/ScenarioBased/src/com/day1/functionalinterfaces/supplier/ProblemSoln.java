6d804ddde9f17ff1a70fc6d4bac779d023de78f9
riteshthakre1 <riteshthakre976@gmail.com>
2026-02-04 23:58:37 +0530

    [Ritesh] : added supplier in functionalinterfaces

---
+++ java8-programming-practice/scenario-based-codebase/ScenarioBased/src/com/day1/functionalinterfaces/supplier/ProblemSoln.java
@@ -0,0 +1,31 @@
+package com.day1.functionalinterfaces.supplier;
+
+import java.time.LocalDateTime;
+import java.util.Random;
+import java.util.function.Supplier;
+
+public class ProblemSoln {
+	public static void main(String[] args) {
+		Supplier<String> otp = () -> {
+			return String.valueOf(10000 + new Random().nextInt(9000));
+		};
+		System.out.println("1. OTP: " + otp.get());
+
+		// 2. Random student ID (STU-XXXX)
+		Supplier<String> studentId = () -> "STU-" + (1000 + new Random().nextInt(9000));
+		System.out.println("2. Student ID: " + studentId.get());
+
+		// 3. Current date-time
+		Supplier<LocalDateTime> now = LocalDateTime::now;
+		System.out.println("3. Current time: " + now.get());
+
+		// 4. Default welcome message
+		Supplier<String> welcome = () -> "Hello! Welcome to our application.";
+		System.out.println("4. " + welcome.get());
+
+		// 5. Random discount coupon (10-50%)
+		Supplier<Integer> discount = () -> 10 + new Random().nextInt(41);
+		System.out.println("5. Your discount coupon: " + discount.get() + "% OFF");
+
+	}
+}
